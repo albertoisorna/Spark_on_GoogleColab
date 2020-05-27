099122e764ea44424c67dcf521c97941a5844da3
Alberto Isorna <alberto_algon@hotmail.com>
2020-05-27 12:25:55 +0200

    Bash Script to install all dedendencies

---
+++ install-all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt install python3-pip -y
+sudo python3 -m pip install homeassistant
+sudo pip3 install py4j
+sudo apt install openjdk-8-jdk -y
+sudo apt-get install scala -y
+wget -P $HOME https://archive.apache.org/dist/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz  
+sudo tar -zxvf $HOME/spark-2.4.5-bin-hadoop2.7.tgz -C $HOME
+sudo pip3 install findspark
+sudo apt install unzip
+sudo apt-get install netcat
+sudo update-alternatives --config java
+sudo update-alternatives --config javac
